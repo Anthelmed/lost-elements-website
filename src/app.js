91b0185d0214750eb756d8a3ce6c68fe7d9876c5
anthelme_dumont <anthelme.dumont@gmail.com>
2016-08-11 01:32:13 +0200

    v2

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ class App {
 
   constructor() {
     this.start();
-    this.sectionName = ['home', 'narration', 'teaser', 'concept-a', 'concept-b', 'demo', 'team']
+    this.sectionName = ['home', 'narration', 'teaser', 'concept-a', 'concept-b', 'demo', 'team',
+      'chapter-wind', 'chapter-earth', 'chapter-water', 'chapter-sun',
+      'chapter-water-platform-1', 'chapter-water-platform-2', 'chapter-water-platform-3', 'chapter-water-sum-up'];
+    this.ascensor = null;
   }
 
   start() {
@@ -15,12 +18,11 @@ class App {
       let hash = window.location.hash.split("#")[1];
       let sectionIndex = this.sectionName.indexOf(hash) || 0;
 
-      console.log(hash);
-
       this.initAscensor();
       this.initParallax();
       this.handleScroll(sectionIndex);
       this.handleEmbedClick();
+      this.handleButtonClick();
     });
   }
 
@@ -29,19 +31,27 @@ class App {
   /////
 
   initAscensor() {
-    let ascensor = $('main').ascensor({
+    this.ascensor = $('main').ascensor({
       ascensorFloorName: this.sectionName,
       childType: 'section',
       width: '100vw',
       height: '100vh',
-      direction: [[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0]],
+      direction: [[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[4,1],[4,2],[4,3],[4,4],[5,3],[6,3],[7,3],[8,3]],
       time: 700,
       wheelNavigation: true,
       wheelNavigationDelay: 20
     });
 
-    ascensor.on("scrollStart", (e, floor) => {
-      this.handleScroll(floor.to);
+    this.ascensor.on("scrollStart", (e, floor) => {
+      let index = floor.to;
+
+      if(index < 7) {
+        $('#nav-side').show();
+        this.handleScroll(index);
+      } else {
+        $('#nav-side').hide();
+      }
+
     });
   }
 
@@ -56,7 +66,7 @@ class App {
   /////
 
   handleEmbedClick() {
-    $('.embed-responsive').on('click', (event) => {
+    $('.embed-responsive').click( (event) => {
       let $this = $(event.target);
       let iframe = $this.find('iframe')[0].contentWindow;
 
@@ -68,10 +78,19 @@ class App {
     });
   }
 
+  handleButtonClick() {
+    $('[data-scrollToDirection]').click( (event) => {
+      let ascensorInstance = this.ascensor.data('ascensor');
+      let direction = $(event.target).attr('data-scrollToDirection');
+
+      ascensorInstance.scrollToDirection(direction);
+    });
+  }
+
   handleScroll(index) {
     let navSidePath = $('#nav-side .path')[0];
     let navSidePathLength = navSidePath.getTotalLength();
-    let sectionLength = $('section').length;
+    let sectionLength = $('.section').length;
     let progress = (navSidePathLength / sectionLength * (index + sectionLength + 1));
 
     TweenMax.to("#nav-side .path", 0.7, {
